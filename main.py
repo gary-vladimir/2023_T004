59a440c229cd5acafa125cbae092621fde98281c
gary-vladimir <gary@garybricks.com>
2023-04-08 14:35:47 -0600

    added move() and stop()

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from cyberpi import *
 from time import sleep
 
+def move(left, right):
+    mbot2.drive_power(left, right)
+
+def stop():
+    mbot2.EM_stop(port="all")
+
 @event.start
 def init():
     ultrasonic2.led_show([80,80,80,80,80,80,80,80], index=1)
@@ -9,4 +15,10 @@ def init():
     console.print("start up successful")
     led.on(50,50,50)
     
-    
+@event.is_press("left")
+def left():
+    move(80,80)
+    sleep(1)
+    move(50,-50)
+    sleep(1)
+    stop()
